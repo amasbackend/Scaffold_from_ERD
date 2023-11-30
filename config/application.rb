e9d00a7800625290f83a32ef09e1218abe4e7547
Rita Chou <rita@amastek.com>
2023-11-30 18:03:56 +0800

    feat - add active_storage & rspec

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ Bundler.require(*Rails.groups)
 module Workflow
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 7.1
 
     # 時區設定
     config.time_zone = "Taipei"
@@ -31,11 +31,11 @@ module Workflow
     config.i18n.default_locale = "zh-TW"
     config.i18n.available_locales = [:"zh-TW"]
     config.i18n.load_path += Dir[Rails.root.join("config/locales/**/*.{rb,yml}")]
-  
+
     # 自動載入資料匣裡的檔案
     config.autoload_paths += ["#{config.root}/lib"]
 
-   # 將 rails generator 預設建立的檔案設為不自動生成
+    # 將 rails generator 預設建立的檔案設為不自動生成
     config.generators do |g|
       g.stylesheets     false
       g.javascripts     false
